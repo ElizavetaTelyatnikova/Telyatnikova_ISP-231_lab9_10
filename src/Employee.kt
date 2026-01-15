81e712cc890d8bd9acbc9243081af01de2841c1e
ElizavetaTelyatnikova <ttelatnikova1@yandex.ru>
2026-01-15 21:20:41 +0300

    Implement assignTask method with validation

--- src/Employee.kt
+++ src/Employee.kt
@@ -4,6 +4,7 @@ class Employee(
     salary: Int,
     yearsOfExperience: Int
 ): ReportGenerator {
+    var currentTask: Task? = null
     var fullName: String = fullName
         get() = field
     var position: String = position
@@ -31,18 +32,35 @@ class Employee(
     override fun generateReport(): String {
         return "=== Данные о сотруднике ===\n ФИО: $fullName\n Должность: $position\n Зарплата: $salary\n Опыт работы: $yearsOfExperience"
     }
+    fun assignTask(newTask: Task){
+        val c = currentTask
+        if (c != null && c.isCompleted){
+            println("Сотрудник уже занят задачей ${c.title}")
+        }else{
+            currentTask = newTask
+            println("Задача ${newTask.title} назначена")
+        }
+    }
 }
 fun main() {
     val employee = Employee("Telyatnikova Elizaveta Petrovna","Manager",20000,30)
+    val task1 = Task("Homework","Do some works",Priority.medium,true)
+    val task2 = Task("Play computer games","Play CS, Dota 2",Priority.low)
     println("Данные сотрудника:")
     println("ФИО: ${employee.fullName}")
     println("Должность: ${employee.position}")
     println("Зарплата: ${employee.salary}")
     println("Опыт работы: ${employee.yearsOfExperience}")
     println()
-    println("Проверка с неккоректными данными")
+    println("Проверка с некорректными данными")
     employee.salary = -2000
     employee.yearsOfExperience = 70
     println("Зарплата: ${employee.salary}")
     println("Опыт работы: ${employee.yearsOfExperience}")
+    println()
+    val developmentDepartment = DevelopmentDepartment()
+    println(developmentDepartment.generateReport())
+    println()
+    employee.assignTask(task1)
+    employee.assignTask(task2)
 }
